4e5b3bcae7d070f5eac68920e56b446db6445546
ozkavosh <augustoslva@hotmail.com>
2023-07-08 12:01:34 -0300

    Link fixes and loader

--- js/script.js
+++ js/script.js
@@ -1,21 +1,34 @@
 $.fn.isInViewport = function () {
-    const elementTop = $(this).offset().top;
-    const elementBottom = elementTop + $(this).outerHeight();
+  const elementTop = $(this).offset().top;
+  const elementBottom = elementTop + $(this).outerHeight();
 
-    const viewportTop = $(window).scrollTop();
-    const viewportBottom = viewportTop + $(window).height();
+  const viewportTop = $(window).scrollTop();
+  const viewportBottom = viewportTop + $(window).height();
 
-    return elementBottom > viewportTop && elementTop < viewportBottom;
-}
-
-const handleNavBar = () => {
-    if($("#banner").isInViewport() && $("#header").hasClass("fixedNav")){
-        $("#header").removeClass("fixedNav");
-    }else if(!$("#banner").isInViewport() && !$("#header").hasClass("fixedNav")){
-        $("#header").addClass("fixedNav");
-    }
+  return elementBottom > viewportTop && elementTop < viewportBottom;
 };
 
-document.addEventListener('scroll', (e) => {
-    handleNavBar();
-});
+const handleNavBar = () => {
+  if ($("#banner").isInViewport() && $("#header").hasClass("fixedNav")) {
+    $("#header").removeClass("fixedNav");
+  } else if (
+    !$("#banner").isInViewport() &&
+    !$("#header").hasClass("fixedNav")
+  ) {
+    $("#header").addClass("fixedNav");
+  }
+};
+
+const handleFooterButtonClick = () => {
+  location.href = location.pathname.includes("index")
+    ? "./pages/contacto.html"
+    : "../pages/contacto.html";
+};
+
+const handleLoader = () => {
+    $(".loaderWrapper").fadeOut("slow");
+}
+
+$(document).ready(handleLoader);
+$(".footerButton").click(handleFooterButtonClick);
+$(document).scroll(handleNavBar);
